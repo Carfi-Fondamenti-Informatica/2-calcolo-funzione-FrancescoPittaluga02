3156655713ece423f4d844e011f437ea5294e0b1
FrancescoPittaluga02 <100779816+FrancescoPittaluga02@users.noreply.github.com>
2022-03-31 23:04:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-   return 0;
+    float a=0,b=0,x=0,y=0,z=0;
+    if(x < 0,y > 0){
+    z=a*x-b*y;
+    std::cout << z <<std::endl;
+    }else if(x>=0,y<=0){
+    z= a*x*x - b*y;
+    std::cout<< z <<std::endl;
+    }else if(x>=0,y>=0) {
+        z = a * x + b * y * y;
+        std::cout << z << std::endl;
+    }else if(x<0,y<0){
+        z = a * x + b * y * y;
+        std::cout << z << std::endl;
+    }
+    return 0;
 }
